55ad22bc877eae923a01deff90eae67f420c1412
ruslanvs <ruslanvs@yahoo.com>
2018-08-03 19:03:03 +0300

    getters setters for classes

--- es6.js
+++ es6.js
@@ -2,3 +2,54 @@ const name = 'John';
 const sentence = `Hi ${name},
 how are you today?`;
 console.log(sentence);
+
+// Shorthand assignment
+const x = 3;
+const y = 5;
+const dict = { x, y };
+console.log(dict);
+
+// Defining methods inside object literals
+const foo = {
+  tada(z) {
+    return z + 1;
+  },
+};
+console.log(foo.tada(1));
+
+// Super calls for inherited methods
+const bar = {
+  toString() {
+    return `${super.toString} with bar!`;
+  },
+};
+console.log(bar.toString());
+
+// Computed properties
+const prop = 'Peter';
+const d = {
+  [prop]: `value for ${prop}`,
+};
+console.log(d);
+
+class Jedi {
+  constructor() {
+    this.forceIsDark = false;
+  }
+
+  toString() {
+    return (`${this.forceIsDark ? 'Join' : 'Fear is the path to'} the dark side`);
+  }
+}
+
+class Sith extends Jedi {
+  constructor() {
+    super();
+    this.forceIsDark = true;
+  }
+}
+
+const joda = new Jedi();
+const darth = new Sith();
+console.log(joda.toString());
+console.log(darth.toString());
